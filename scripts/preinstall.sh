fb29fc656823e66749399248e7cbb630ae6fb7af
Haider Zaman <haiderzamanusafxay@gmail.com>
2024-01-22 17:04:42 +0500

    init

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,21 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 function cocoapods() {
     echo "Installing Cocoapods"
-    if [[ $(uname -p) == 'arm' ]]; then
-      npm run ios-gems-m1 &> /dev/null || exit 1
-    else
-      npm run ios-gems &> /dev/null  || exit 1
-    fi
-    
+    npm run ios-gems &> /dev/null || npm run ios-gems-m1 &> /dev/null || exit 1
 }
 
+# Check if running on Windows
+if [[ "$OSTYPE" == "msys" ]]; then
+  echo "This script is not intended for Windows."
+  exit 1
+fi
+
+# Check if running on macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
+  # Check and install Bundler if not present
   if !(gem list bundler -i --version 2.3.26) > /dev/null 2>&1; then
     echo "Installing Bundler"
-    gem install bundler --version 2.3.26  || exit 1
+    gem install bundler --version 2.3.26 || exit 1
   fi
 
+  # Check and install Cocoapods if not present
   if !(gem list cocoapods -i --version 1.14.3) > /dev/null 2>&1; then
     cocoapods
   fi
